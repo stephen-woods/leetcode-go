d340c1ef80158421afe3d9404dec97541287f464
Woods, Stephen (Contractor) <stephen_woods@cable.comcast.com>
2025-08-03 18:50:49 -0400

    27 remove element

--- problems/88_merge_sorted_array/merged_sorted_array_test.go
+++ problems/88_merge_sorted_array/merged_sorted_array_test.go
@@ -7,11 +7,14 @@ import (
 
 // Example 1:
 //
-// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3 Output:
-// [1,2,2,3,5,6] Explanation: The arrays we are merging are [1,2,3] and
-// [2,5,6]. The result of the merge is [1,2,2,3,5,6] with the underlined
-// elements coming from nums1.
-func TestMerge1(t *testing.T) {
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6] 
+//
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6]. The result
+// of the merge is [1,2,2,3,5,6] with the underlined elements coming from
+// nums1.
+
+func TestMergedSortedArray1(t *testing.T) {
 	nums1 := []int{1, 2, 3, 0, 0, 0}
 	m := 3
 	nums2 := []int{2, 5, 6}
@@ -27,9 +30,12 @@ func TestMerge1(t *testing.T) {
 
 // Example 2:
 //
-// Input: nums1 = [1], m = 1, nums2 = [], n = 0 Output: [1] Explanation: The
-// arrays we are merging are [1] and []. The result of the merge is [1].
-func TestMerge2(t *testing.T) {
+// Input: nums1 = [1], m = 1, nums2 = [], n = 0
+// Output: [1] 
+//
+// Explanation: The arrays we are merging are [1] and []. The result of the
+// merge is [1].
+func TestMergedSortedArray2(t *testing.T) {
 	nums1 := []int{1}
 	m := 1
 	nums2 := []int{}
@@ -45,11 +51,13 @@ func TestMerge2(t *testing.T) {
 
 // Example 3: 
 //
-// Input: nums1 = [0], m = 0, nums2 = [1], n = 1 Output: [1] Explanation: The
-// arrays we are merging are [] and [1]. The result of the merge is [1]. Note
-// that because m = 0, there are no elements in nums1. The 0 is only there to
-// ensure the merge result can fit in nums1.
-func TestMerge3(t *testing.T) {
+// Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+// Output: [1]
+//
+// Explanation: The arrays we are merging are [] and [1]. The result of the
+// merge is [1]. Note that because m = 0, there are no elements in nums1. The 0
+// is only there to ensure the merge result can fit in nums1.
+func TestMergedSortedArray3(t *testing.T) {
 	nums1 := []int{0}
 	m := 0 
 	nums2 := []int{1}
